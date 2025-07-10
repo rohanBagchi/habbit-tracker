c1fb3b10524afc4342eb673932c61e65e94fb43b
Rohan Bagchi <rohan.bagchi@zalando.de>
2025-07-10 18:20:51 +0200

    making logoput button better

--- src/SignOut.tsx
+++ src/SignOut.tsx
@@ -1,7 +1,17 @@
 import { useAuthActions } from '@convex-dev/auth/react';
 import { Button } from './components/ui/button';
+import { LogOut } from 'lucide-react';
 
 export function SignOut() {
   const { signOut } = useAuthActions();
-  return <Button onClick={() => void signOut()}>Sign out</Button>;
+  return (
+    <Button
+      variant='secondary'
+      size='icon'
+      className='size-8'
+      onClick={() => void signOut()}
+    >
+      <LogOut />
+    </Button>
+  );
 }
